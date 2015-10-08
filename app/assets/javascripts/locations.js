3742e0c7739f2a349b48a695a307c370317fe64c
Sally Moore <sallyamoore@gmail.com>
2015-10-08 16:49:48 -0700

    reformatting js files to make testing easier.

---
+++ app/assets/javascripts/locations.js
@@ -0,0 +1,36 @@
+// var overpassPrefix = "http://overpass-api.de/api/interpreter?data=",
+//   map;
+
+$(document).ready(function() {
+  var map = new Map({
+    overpassPrefix: "http://overpass-api.de/api/interpreter?data=",
+    mapboxPk: "pk.eyJ1Ijoic2FsbHlhbW9vcmUiLCJhIjoiY2lmZm53MmlkOHA2YnNka25wd3BmNDB3dyJ9.F5FdTfUY5XLbzWMcWpRp2A",
+    baseMap: 'sallyamoore.nkikgok3',
+    startLatLon: [52.3081, 4.7642],
+    startZoom: 12,
+    bikeMapLayer: 'http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png',
+    markerFormat: {
+      color: "#01D298",
+      opacity: 1.0,
+      fill: false,
+      weight: 3.5,
+      radius: 10
+    }
+  });
+  map.loadMap();
+})
+
+// finds current location.
+function onLocationFound(event) {
+    var radius = event.accuracy / 2;
+
+    L.marker(event.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(event.latlng, radius).addTo(map);
+}
+
+// error if current location can't be found
+function onLocationError(event) {
+    alert(event.message);
+}
---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,51 @@
+(function(exports) {
+  "use strict";
+
+  function Map(options) {
+    this.overpassPrefix = options.overpassPrefix;
+    this.mapboxPk = options.mapboxPk;
+    this.baseMap = options.baseMap;
+    this.startLatLon = options.startLatLon;
+    this.startZoom = options.startZoom;
+    this.bikeMapLayer = options.bikeMapLayer;
+    this.markerFormat = options.markerFormat;
+  }
+  exports.Map = Map;
+
+  Map.prototype = {
+    loadMap: function() {
+      L.mapbox.accessToken = this.mapboxPk;
+      var new_map = L.mapbox.map('map', this.baseMap)
+        .setView(this.startLatLon, this.startZoom);
+      L.tileLayer(this.bikeMapLayer).addTo(new_map);
+      this.getBounds(new_map);
+    },
+
+    getBounds: function(map) {
+      var bounds = [ map.getBounds()._southWest.lat,
+        map.getBounds()._southWest.lng,
+        map.getBounds()._northEast.lat,
+        map.getBounds()._northEast.lng
+      ];
+      this.apiCall(map, bounds);
+    },
+
+    apiCall: function(map, bounds) {
+      var markerFormat = this.markerFormat;
+      $.getJSON(this.overpassPrefix + '[out:json];(relation["type"="network"]["network"="rcn"]('
+        + (bounds.join()) +
+        ');node(r)->.nodes;rel(r);way(r););out body;>;out skel qt;',
+        function(data) {
+          // create clickable markers for each bike node in bounds
+          for (var i = 0; i < data.elements.length - 1; i++) {
+            if (data.elements[i].tags && data.elements[i].lat)  {
+              new L.circleMarker(
+                [data.elements[i].lat, data.elements[i].lon],
+                markerFormat)
+              .addTo(map);
+            }
+          }
+        });
+    }
+  };
+})(this);
