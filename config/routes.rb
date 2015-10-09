75e23b7c08b4a21760dca6ac1b075c07e14226c8
Sally Moore <sallyamoore@gmail.com>
2015-10-09 15:54:33 -0700

    undoing prior js test stuff. gonna try something else.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  mount MochaRails::Engine => 'mocha' unless Rails.env.production?
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
