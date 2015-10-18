38d622968aa8b48a815b69bb2ee6e0bb605209c7
Sally Moore <sallyamoore@gmail.com>
2015-10-18 10:43:45 -0700

    Finds and displays location from search query.

--- app/assets/javascripts/map-render.js
+++ app/assets/javascripts/map-render.js
@@ -13,34 +13,59 @@ $(document).ready(function() {
     attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>'
   });
 
+  var trashToCollect = '.bad-query, .zoom-alert';
+  var alertContent = {
+    badQuery:  {
+      class: 'bad-query',
+      text: 'No matching locations! Check your query and try again.'
+    },
+    zoomAlert: {
+      class: 'zoom-alert',
+      text: 'Zoom in to view clickable nodes.' }
+  };
+
   map.loadMap(function(result) {
     $(".find-location").click(function(event) {
       event.preventDefault();
+      collectTrash(trashToCollect);
       toggleSearchForm();
     });
 
     $(".location-submit").click(function(event) {
-      $('.bad-query').remove();
       event.preventDefault();
+      collectTrash(trashToCollect);
       var locationQuery = $("input.location-query").val();
       // var geocoder = L.mapbox.geocoder('mapbox.places');
       // geocoder.query(locationQuery, function(res){
       //   console.log(res);
-      // });
-
+      // })
       var latlng = map.startLatLon.join(",");
       var geocodeQuery = "https://api.mapbox.com/v4/geocode/mapbox.places/"
         + locationQuery + ".json?proximity=" + latlng + "&access_token="
         + map.mapboxPk
+
       $.getJSON(geocodeQuery, function(data) {
+        // returns array of place objects
+        // each object has bbox, center [lat,lon], id, place_name and other attrs
+        // For now, I map to first one (closest match to startLatLon).
+        // Possible refactor: Show list of place objects and user selects.
         if (data && data.features.length !== 0) {
-          console.log(data.features); // returns array of place objects
-          // each object has bbox, center [lat,lon], id, place_name and other attrs
+          toggleSearchForm();
+
+          // returns 'center' in format [lon, lat], so must reverse
+          var dataLatLng = data.features[0].center.reverse();
+
+          // for first object, add clickable marker (display place name as popup)
+          L.marker(dataLatLng).addTo(map.osm_map)
+            .bindPopup(data.features[0].place_name);
+
+          // move center of map to location
+           map.osm_map.setView(dataLatLng, map.startZoom);
         } else {
           var badQueryAlert = document.createElement('div');
-          badQueryAlert.className = 'alert alert-danger bad-query';
+          badQueryAlert.className = 'alert alert-danger ' + alertContent.badQuery.class;
           document.getElementsByClassName('alerts-div')[0].appendChild(badQueryAlert);
-          $(badQueryAlert).text("No results. Try a different query.");
+          $(badQueryAlert).text(alertContent.badQuery.text);
         }
       });
 
@@ -48,22 +73,23 @@ $(document).ready(function() {
 
     $(".my-location").click(function(event) {
       event.preventDefault();
+      collectTrash(trashToCollect);
       result.locate({setView: true, maxZoom: 16});
       result.on('locationfound', onLocationFound);
       result.on('locationerror', onLocationError);
     });
 
     result.on('zoomend, moveend', function(event) {
-      $('.zoom-alert').remove();
+      collectTrash(trashToCollect);
       var zoom = result.getZoom();
       if (zoom > 11) {
         map.findBounds(map.osm_map);
       } else {
         $('.css-icon').remove();
         var zoomAlert = document.createElement('div');
-        zoomAlert.className = 'alert alert-warning zoom-alert';
+        zoomAlert.className = 'alert alert-warning ' + alertContent.zoomAlert.class;
         document.getElementsByClassName('alerts-div')[0].appendChild(zoomAlert);
-        $(zoomAlert).text("Zoom in to view clickable nodes.");
+        $(zoomAlert).text(alertContent.zoomAlert.text);
       }
     });
 
@@ -72,11 +98,9 @@ $(document).ready(function() {
 });
 
 function toggleSearchForm() {
-  $('.bad-query').remove();
   $(".location-search").slideToggle();
 }
 
-function showPlaceResults() {
-  // slide up .location-search just a little to show options below
-  //
+function collectTrash(trashToCollect) {
+  $(trashToCollect).remove();
 }
