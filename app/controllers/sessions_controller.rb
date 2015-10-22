ba3429921ddc86deb5cc269ec0f951d09fc1438b
Sally Moore <sallyamoore@gmail.com>
2015-10-22 09:38:18 -0700

    Refactored sessions_controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,29 +2,10 @@ class SessionsController < ApplicationController
 
   def create
     auth_hash = request.env['omniauth.auth'] || params
-
     if auth_hash["uid"]
-      user = User.find_or_create_from_omniauth(auth_hash)
+      login_by_provider(auth_hash)
     elsif auth_hash[:session] && auth_hash[:session][:username]
-      user = User.find_by(username: auth_hash[:session][:username])
-      # Add email option?
-      # || User.find_by(email: auth_hash[:session][:email])
-    end
-
-    if user && auth_hash["uid"]
-      login_user(user)
-      redirect_to root_path, flash: { success: MESSAGES[:success] }
-
-    elsif user && user.authenticate(auth_hash[:session][:password])
-      if user.activated?
-        login_user(user)
-        # params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_to root_path, flash: { success: MESSAGES[:success] }
-      else
-        flash[:alert] = MESSAGES[:not_activated]
-        redirect_to root_url
-      end
-
+      login_by_username(auth_hash)
     else
       redirect_to root_path, flash: { error: MESSAGES[:failed_authentication] }
     end
@@ -33,6 +14,33 @@ class SessionsController < ApplicationController
   def destroy
     reset_session
     redirect_to root_path
+  end
+
+  private
+
+  def login_by_provider(auth_hash)
+    user = User.find_or_create_from_omniauth(auth_hash)
+    if user
+      login_user(user)
+      redirect_to root_path, flash: { success: MESSAGES[:success] }
+    end
+
+  end
+
+  def login_by_username(auth_hash)
+    user = User.find_by(username: auth_hash[:session][:username])
+    # Add email option?
+    # || User.find_by(email: auth_hash[:session][:email])
+    if user && user.activated? && user.authenticate(auth_hash[:session][:password])
+      login_user(user)
+      # params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_to root_path, flash: { success: MESSAGES[:success] }
+    elsif user && !user.activated?
+      flash[:alert] = MESSAGES[:not_activated]
+      redirect_to root_url
+    else
+      redirect_to root_path, flash: { error: MESSAGES[:failed_authentication] }
+    end
   end
 
 end
